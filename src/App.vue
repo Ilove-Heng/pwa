825430557b1feaad7082299a575b3f44c269778f
ILove <nangseakheng168@gmail.com>
2023-05-17 21:11:20 +0700

    pwa

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <div>Hello Kon PAPA</div>
+    <img class="img" src="@/assets/img.jpg" alt="img" />
+  </div>
+</template>
+
+<style scoped>
+.img {
+  height: 200px;
+  margin-top: 20px;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
